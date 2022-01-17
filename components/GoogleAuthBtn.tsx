0a498bbc90627e0693ea0054125d9d5f48dfdd06
bineetNaidu <bineet.s.naidu@gmail.com>
2022-01-17 15:47:16 +0530

    🥮 add Google Auth Button Component

---
+++ components/GoogleAuthBtn.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+export const GoogleAuthBtn: FC = () => {
+  return (
+    <button className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+      Sign in with Google
+    </button>
+  );
+};
