ab791bbacee56eca9c6da7cfb8cce96b3b983d65
bineetNaidu <bineet.s.naidu@gmail.com>
2022-01-17 20:48:00 +0530

    🎞️ add react notification provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ToastProvider } from 'react-toast-notifications';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <div className="min-h-screen h-full container mx-auto py-4">
-        <Component {...pageProps} />
-      </div>
+      <ToastProvider placement="top-right">
+        <div className="min-h-screen h-full container mx-auto py-4">
+          <Component {...pageProps} />
+        </div>
+      </ToastProvider>
     </>
   );
 }
