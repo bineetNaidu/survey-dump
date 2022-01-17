b499c0427fd70b21a8caa76ff9b8f6f9a17c12c6
bineetNaidu <bineet.s.naidu@gmail.com>
2022-01-17 22:21:55 +0530

    ✨ config next auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ToastProvider } from 'react-toast-notifications';
+import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ToastProvider placement="top-right">
-        <div className="min-h-screen h-full container mx-auto py-4">
-          <Component {...pageProps} />
-        </div>
-      </ToastProvider>
+      <SessionProvider session={pageProps.session}>
+        <ToastProvider placement="top-right">
+          <div className="min-h-screen h-full container mx-auto py-4">
+            <Component {...pageProps} />
+          </div>
+        </ToastProvider>
+      </SessionProvider>
     </>
   );
 }
