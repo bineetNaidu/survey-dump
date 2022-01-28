881fd18417fc50aab79923746e1de1c648ac5bbf
bineetNaidu <bineet.s.naidu@gmail.com>
2022-01-28 22:13:29 +0530

    🛩️ add option edit/delete handler

--- web/components/EditableQuestionCard.tsx
+++ web/components/EditableQuestionCard.tsx
@@ -6,6 +6,8 @@ import { Formik, Form } from 'formik';
 import {
   useUpdateQuestionMutation,
   useDeleteQuestionMutation,
+  useUpdateOptionMutation,
+  useDeleteOptionMutation,
 } from '../lib/graphql';
 import { useToasts } from 'react-toast-notifications';
 
@@ -22,6 +24,9 @@ export const EditableQuestionCard: FC<EditableQuestionCardProps> = ({
   const [updateQuestion] = useUpdateQuestionMutation();
   const [deleteQuestion, { loading: deleteQuestionLoading }] =
     useDeleteQuestionMutation();
+  const [updateOption] = useUpdateOptionMutation();
+  const [deleteOption, { loading: deleteOptionLoading }] =
+    useDeleteOptionMutation();
   const { addToast } = useToasts();
 
   const handleDeleteQuestion = async () => {
@@ -37,6 +42,28 @@ export const EditableQuestionCard: FC<EditableQuestionCardProps> = ({
       });
     } else {
       addToast('Error deleting question', {
+        appearance: 'error',
+        autoDismiss: true,
+      });
+    }
+  };
+
+  const handleDeleteOption = async (optionId: string) => {
+    const { data } = await deleteOption({
+      variables: {
+        deleteOptionId: optionId,
+        questionId: q._id,
+      },
+    });
+
+    if (data?.deleteOption) {
+      // delete it from store
+      addToast('Option deleted', {
+        appearance: 'success',
+        autoDismiss: true,
+      });
+    } else {
+      addToast('Error deleting option', {
         appearance: 'error',
         autoDismiss: true,
       });
@@ -76,7 +103,7 @@ export const EditableQuestionCard: FC<EditableQuestionCardProps> = ({
         {({ getFieldProps, isSubmitting, handleSubmit }) => (
           <Form>
             <div
-              className={`absolute top-7 left-8 -rotate-90 opacity-0 ${
+              className={`absolute top-[30%] left-8 -rotate-90 opacity-0 ${
                 isEditing && 'group-hover:opacity-100 group-hover:-left-8'
               } flex flex-col transition-all duration-500 translate-y-2 -translate-x-5`}
             >
@@ -118,7 +145,6 @@ export const EditableQuestionCard: FC<EditableQuestionCardProps> = ({
                 </p>
               )}
             </div>
-
             {q.isField &&
               (isEditing ? (
                 <input
@@ -137,24 +163,6 @@ export const EditableQuestionCard: FC<EditableQuestionCardProps> = ({
                   className={`w-full px-2 py-1 border border-gray-400 outline-none mt-2 rounded`}
                 />
               ))}
-            {q.isOption && (
-              <div className="flex flex-col">
-                {q.options.map((option) => (
-                  <div key={option._id}>
-                    {option.other ? (
-                      <input type="text" placeholder={option.other} />
-                    ) : (
-                      <div className="flex items-center">
-                        <input type="radio" value={option.name!} />
-                        <p className="text-sm font-semibold ml-2 my-1">
-                          {option.name!}
-                        </p>
-                      </div>
-                    )}
-                  </div>
-                ))}
-              </div>
-            )}
             <div
               className={`flex justify-end ${
                 isEditing ? 'opacity-100' : 'opacity-0 pointer-events-none'
@@ -186,6 +194,123 @@ export const EditableQuestionCard: FC<EditableQuestionCardProps> = ({
           </Form>
         )}
       </Formik>
+      {q.isOption && (
+        <div
+          className={`flex flex-col transition-all ${!isEditing && '-mt-6 '}`}
+        >
+          {q.options.map((option) => (
+            <div key={option._id}>
+              <Formik
+                initialValues={{}}
+                onSubmit={async (values, { setSubmitting }) => {
+                  setSubmitting(true);
+                  const { data } = await updateOption({
+                    variables: {
+                      updateOptionId: option._id,
+                      data: {
+                        ...values,
+                      },
+                    },
+                  });
+                  if (data?.updateOption) {
+                    // **update the option in store**
+                    addToast('Option updated', {
+                      appearance: 'success',
+                      autoDismiss: true,
+                    });
+                    setIsEditing(false);
+                    setSubmitting(false);
+                  } else {
+                    addToast('Error updating option', {
+                      appearance: 'error',
+                      autoDismiss: true,
+                    });
+                  }
+                }}
+              >
+                {({ handleSubmit, getFieldProps, isSubmitting }) => (
+                  <Form>
+                    {option.other ? (
+                      <>
+                        {isEditing ? (
+                          <input
+                            type="text"
+                            defaultValue={option.other}
+                            {...getFieldProps('other')}
+                            placeholder="to update type or leave it blank"
+                            disabled={!isEditing}
+                            className={`w-full px-2 py-1 border border-dashed border-gray-400 outline-none mt-2 rounded`}
+                          />
+                        ) : (
+                          <input
+                            type="text"
+                            disabled
+                            placeholder={option.other}
+                          />
+                        )}
+                      </>
+                    ) : (
+                      <div className="flex items-center">
+                        <input type="radio" disabled />
+                        {isEditing ? (
+                          <>
+                            <input
+                              type="text"
+                              defaultValue={option.name!}
+                              {...getFieldProps('name')}
+                              placeholder="to update type or leave it blank"
+                              disabled={!isEditing}
+                              className={`w-fit active:border-blue-400 px-2 py-1 text-sm border border-dashed border-gray-400 outline-none mt-2 rounded ${
+                                isEditing && 'ml-2'
+                              }`}
+                            />
+                            <div>
+                              <button
+                                type="submit"
+                                onClick={async (e) => {
+                                  e.preventDefault();
+                                  await handleSubmit();
+                                }}
+                                disabled={isSubmitting}
+                                className="text-xs text-gray-600 border border-gray-500 hover:bg-gray-400 hover:border-transparent hover:text-white font-bold transition-all duration-300 py-1 px-2 rounded mt-2 mx-2 disabled:opacity-50"
+                              >
+                                {isSubmitting ? (
+                                  <BiLoaderCircle className="animate-spin" />
+                                ) : (
+                                  'update!'
+                                )}
+                              </button>
+                              <button
+                                type="button"
+                                onClick={async (e) => {
+                                  e.preventDefault();
+                                  await handleDeleteOption(option._id);
+                                }}
+                                disabled={isSubmitting}
+                                className="text-xs text-gray-600 border border-gray-500 hover:bg-gray-400 hover:border-transparent hover:text-white font-bold transition-all duration-300 py-1 px-2 rounded mt-2 disabled:opacity-50"
+                              >
+                                {deleteOptionLoading ? (
+                                  <BiLoaderCircle className="animate-spin" />
+                                ) : (
+                                  'delete!'
+                                )}
+                              </button>
+                            </div>
+                          </>
+                        ) : (
+                          <p className="text-sm font-semibold ml-2 my-1">
+                            {option.name!}
+                          </p>
+                        )}
+                      </div>
+                    )}
+                  </Form>
+                )}
+              </Formik>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
