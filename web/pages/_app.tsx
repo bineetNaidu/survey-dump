7792f769612109f49e3563832146ceb709915444
bineetNaidu <bineet.s.naidu@gmail.com>
2022-01-28 14:57:18 +0530

    🎉 add Next Page Loader

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,7 +1,24 @@
 import '../styles/globals.css';
+import 'nprogress/nprogress.css';
 import type { AppProps } from 'next/app';
 import { ToastProvider } from 'react-toast-notifications';
 import { SessionProvider } from 'next-auth/react';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+
+NProgress.configure({ showSpinner: true });
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done();
+});
+
+Router.events.on('routeChangeError', () => {
+  NProgress.done();
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
