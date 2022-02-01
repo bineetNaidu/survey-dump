583bc0c062c262d14e00a8fe8577ca9d14a83235
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-01 15:49:12 +0530

    🎉 add user resolver

---
+++ server/src/resolvers/dto/users.dto.ts
@@ -0,0 +1,40 @@
+import { User } from '../../models/User';
+import { InputType, Field, ObjectType } from 'type-graphql';
+
+@InputType()
+export class RegisterInput {
+  @Field()
+  name: string;
+  @Field()
+  email: string;
+  @Field()
+  password: string;
+  @Field({ nullable: true })
+  avatar?: string;
+}
+
+@InputType()
+export class LoginInput {
+  @Field()
+  email: string;
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+export class AuthResponse {
+  @Field({ nullable: true })
+  token?: string;
+  @Field({ nullable: true })
+  user?: User;
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+}
