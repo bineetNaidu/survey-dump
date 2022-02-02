b1bb1d0ec444ab9046aede43d55eaf78be0d6682
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-02 14:59:11 +0530

    🐛 add new domain

--- web/next.config.js
+++ web/next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+    domains: [
+      'lh3.googleusercontent.com',
+      'avatars.githubusercontent.com',
+      'avatars.dicebear.com',
+    ],
   },
 };
 
