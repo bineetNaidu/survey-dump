a921712af70900e8d886043017793a00cc0ae9aa
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-02 11:59:37 +0530

    🚄 update user store n its types

--- web/lib/types.ts
+++ web/lib/types.ts
@@ -1,9 +1,13 @@
 import { Survey, Question } from './graphql';
 
+type CreatorType = Pick<Survey['creator'], '_id'>;
+
 export type SelectedSurveyType = Pick<
   Survey,
   '_id' | 'title' | 'description' | 'status'
->;
+> & {
+  creator: CreatorType;
+};
 
 export type BaseQuestionType = Omit<Question, 'survey'> & {
   survey: {
@@ -11,6 +15,7 @@ export type BaseQuestionType = Omit<Question, 'survey'> & {
   };
 };
 
-export type BaseSurveyType = Omit<Survey, 'questions'> & {
+export type BaseSurveyType = Omit<Survey, 'questions' | 'creator'> & {
   questions: BaseQuestionType[];
+  creator: CreatorType;
 };
