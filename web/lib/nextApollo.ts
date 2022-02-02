131cdb4e3971b6c92b6fca5c44c683ee5db9f103
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-02 11:58:45 +0530

    🎋 update nextApollo header config to use authorization

--- web/lib/nextApollo.ts
+++ web/lib/nextApollo.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { withApollo as createWithApollo } from 'next-apollo';
 import { setContext } from '@apollo/client/link/context';
+import Cookies from 'js-cookie';
 
 // const uri = process.env.GRAPHQL_ENDPOINT!;
 // console.log(uri);
@@ -9,10 +10,13 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000/graphql',
 });
 
+const token = Cookies.get('__survey_dump_auth_token__');
+
 const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
+      authorization: token ? `Bearer ${token}` : null,
     },
   };
 });
