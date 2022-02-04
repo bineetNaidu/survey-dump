daf0d1c82e31835f494e589a8f2ee04636b78e28
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-04 14:33:45 +0530

    ✨ add deleteUser handler

--- web/pages/dashboard/settings.tsx
+++ web/pages/dashboard/settings.tsx
@@ -4,12 +4,54 @@ import { SideNavbar } from '../../components/SideNavbar';
 import { withApollo } from '../../lib/nextApollo';
 import { useUserStore } from '../../lib/stores/users.store';
 import { FaTrash } from 'react-icons/fa';
+import { PromtModal } from '../../components/PromtModal';
+import { useState } from 'react';
+import { useDeleteMeMutation } from '../../lib/graphql';
+import { useApolloClient } from '@apollo/client';
+import { useToasts } from 'react-toast-notifications';
 
 const Settings: NextPage = () => {
   const { authUser } = useUserStore();
+  const [showDeletePromt, setShowDeletePromt] = useState(false);
+  const [deleteMe] = useDeleteMeMutation();
+  const client = useApolloClient();
+  const { addToast } = useToasts();
+
+  const handleDeleteAccount = async () => {
+    try {
+      const { data } = await deleteMe();
+
+      if (!data?.deleteMe) {
+        throw new Error('Could not delete account');
+      }
+
+      await client.resetStore();
+      window.location.href = '/';
+    } catch (error) {
+      addToast((error as Error).message, {
+        appearance: 'error',
+        autoDismiss: true,
+      });
+    }
+  };
+
   return (
     <div className="flex">
       <SideNavbar />
+      {authUser && (
+        <>
+          <PromtModal
+            title="Delete Your Account?"
+            description="Are you sure you want to delete your account? This action cannot be undone."
+            handleDelete={handleDeleteAccount}
+            handleCancel={() => {
+              setShowDeletePromt(false);
+            }}
+            show={showDeletePromt}
+            setShow={setShowDeletePromt}
+          />
+        </>
+      )}
       <div className="md:w-11/12 w-full bg-gray-200 min-h-screen h-full transition-all py-10 px-16">
         <div>
           <h1 className="text-3xl text-blue-600 font-bold">Account Settings</h1>
@@ -63,7 +105,10 @@ const Settings: NextPage = () => {
 
             <div className="w-2/3 border border-red-500 bg-red-100 flex flex-col justify-center items-center mx-auto p-6 rounded mt-8 bg-opacity-20">
               <h1 className="font-bold text-3xl mb-5">Danger Zone</h1>
-              <button className="px-4 py-2 rounded bg-red-500 text-white text-sm flex items-center hover:bg-red-600 transition-all ">
+              <button
+                className="px-4 py-2 rounded bg-red-500 text-white text-sm flex items-center hover:bg-red-600 transition-all"
+                onClick={() => setShowDeletePromt(true)}
+              >
                 <FaTrash className="mr-1" /> Delete my survey dump account
               </button>
             </div>
