da9bee544282731b5c3d803ae56fb31ba328efea
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-04 15:23:48 +0530

    🎉 add updateUser handlers

--- web/pages/dashboard/settings.tsx
+++ web/pages/dashboard/settings.tsx
@@ -5,17 +5,85 @@ import { withApollo } from '../../lib/nextApollo';
 import { useUserStore } from '../../lib/stores/users.store';
 import { FaTrash } from 'react-icons/fa';
 import { PromtModal } from '../../components/PromtModal';
-import { useState } from 'react';
-import { useDeleteMeMutation } from '../../lib/graphql';
+import { useState, useEffect } from 'react';
+import { useDeleteMeMutation, useUpdateMeMutation } from '../../lib/graphql';
 import { useApolloClient } from '@apollo/client';
 import { useToasts } from 'react-toast-notifications';
+import { BiLoaderCircle } from 'react-icons/bi';
 
 const Settings: NextPage = () => {
-  const { authUser } = useUserStore();
+  const { authUser, updateUser } = useUserStore();
   const [showDeletePromt, setShowDeletePromt] = useState(false);
   const [deleteMe] = useDeleteMeMutation();
   const client = useApolloClient();
   const { addToast } = useToasts();
+  const [name, setName] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [isEditingName, setIsEditingName] = useState(false);
+  const [isEditingAvatar, setIsEditingAvatar] = useState(false);
+  const [updateMe, { loading: isLoadingOnUpdateMe }] = useUpdateMeMutation();
+
+  const Loader = () => {
+    return (
+      <div className="flex justify-center items-center">
+        <BiLoaderCircle className="animate-spin" />
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    if (authUser) {
+      setName(authUser.name);
+      setAvatar(authUser.avatar);
+    }
+  }, [authUser]);
+
+  const handleUpdateMeName = async () => {
+    if (name.trim() === '') {
+      addToast('Name is required', { appearance: 'error', autoDismiss: true });
+      return;
+    }
+
+    const { data } = await updateMe({
+      variables: {
+        data: { name },
+      },
+    });
+
+    if (data && data.updateMe) {
+      updateUser(data.updateMe);
+      addToast('Name updated successfully', {
+        appearance: 'success',
+        autoDismiss: true,
+      });
+      setIsEditingName(false);
+    }
+  };
+
+  const handleUpdateMeAvatar = async () => {
+    if (avatar.trim() === '') {
+      addToast('Avatar is required', {
+        appearance: 'error',
+        autoDismiss: true,
+      });
+      return;
+    }
+
+    const { data } = await updateMe({
+      variables: {
+        data: { avatar },
+      },
+    });
+
+    if (data && data.updateMe) {
+      updateUser(data.updateMe);
+      addToast('Avatar updated successfully', {
+        appearance: 'success',
+        autoDismiss: true,
+      });
+      setIsEditingAvatar(false);
+    }
+  };
 
   const handleDeleteAccount = async () => {
     try {
@@ -63,11 +131,45 @@ const Settings: NextPage = () => {
           <div className="flex flex-col mt-6">
             <div className="flex justify-between items-center border-y-2 border-gray-300 px-6 py-2">
               <h2 className="font-medium text-gray-600">Name</h2>
-              <h2 className="font-semibold text-gray-800">{authUser?.name}</h2>
-              <div>
-                <button className="text-blue-500 outline-none border border-transparent hover:border-blue-400 hover:bg-blue-100 transition-all rounded px-2 py-1">
-                  Update
-                </button>
+              {isEditingName ? (
+                <input
+                  className="rounded w-fit bg-transparent border border-gray-500 px-2 py-1 text-gray-700"
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              ) : (
+                <h2 className="font-semibold text-gray-800">
+                  {authUser?.name}
+                </h2>
+              )}
+              <div className="transition-all flex">
+                {isEditingName ? (
+                  <>
+                    <button
+                      className="text-blue-500 outline-none border border-transparent hover:border-blue-400 hover:bg-blue-100 transition-all rounded px-2 py-1 disabled:opacity-50 disabled:cursor-not-allowed"
+                      onClick={() => setIsEditingName(false)}
+                      disabled={isLoadingOnUpdateMe}
+                    >
+                      Cancel
+                    </button>
+                    <div className="border border-gray-300 mx-1"></div>
+                    <button
+                      className="text-blue-500 outline-none border border-transparent hover:border-blue-400 hover:bg-blue-100 transition-all rounded px-2 py-1 disabled:opacity-50 disabled:cursor-not-allowed"
+                      disabled={isLoadingOnUpdateMe}
+                      onClick={handleUpdateMeName}
+                    >
+                      {isLoadingOnUpdateMe ? <Loader /> : 'Save'}
+                    </button>
+                  </>
+                ) : (
+                  <button
+                    className="text-blue-500 outline-none border border-transparent hover:border-blue-400 hover:bg-blue-100 transition-all rounded px-2 py-1"
+                    onClick={() => setIsEditingName(true)}
+                  >
+                    Edit
+                  </button>
+                )}
               </div>
             </div>
 
@@ -75,8 +177,11 @@ const Settings: NextPage = () => {
               <h2 className="font-medium text-gray-600">Email</h2>
               <h2 className="font-semibold text-gray-800">{authUser?.email}</h2>
               <div>
-                <button className="text-blue-500 outline-none border border-transparent hover:border-blue-400 hover:bg-blue-100 transition-all rounded px-2 py-1">
-                  Update
+                <button
+                  className="text-blue-500 outline-none border border-transparent hover:border-blue-400 hover:bg-blue-100 transition-all rounded px-2 py-1 cursor-not-allowed"
+                  disabled
+                >
+                  Edit
                 </button>
               </div>
             </div>
@@ -84,22 +189,51 @@ const Settings: NextPage = () => {
             <div className="flex justify-between items-center border-b-2 border-gray-300 px-6 py-2">
               <h2 className="font-medium text-gray-600">Avatar</h2>
               <div className="flex-1 flex justify-center items-center">
-                <Image
-                  src={authUser ? authUser.avatar : '/vercel.svg'}
-                  height={50}
-                  width={50}
-                  className="rounded-full"
-                  alt={authUser?.name}
-                />
+                {isEditingAvatar ? (
+                  <input
+                    className="rounded w-fit bg-transparent border border-gray-500 px-2 py-1 text-gray-700"
+                    type="text"
+                    value={avatar}
+                    disabled={isLoadingOnUpdateMe}
+                    onChange={(e) => setAvatar(e.target.value)}
+                  />
+                ) : (
+                  <Image
+                    src={authUser ? authUser.avatar : '/assets/defaultUser.png'}
+                    height={50}
+                    width={50}
+                    className="rounded-full"
+                    alt={authUser?.name}
+                  />
+                )}
               </div>
-              <div className="flex">
-                <button className="text-red-500 outline-none border border-transparent hover:border-red-400 hover:bg-red-100 transition-all rounded px-2 py-1">
-                  Remove
-                </button>
-                <div className="border border-gray-300 mx-1"></div>
-                <button className="text-blue-500 outline-none border border-transparent hover:border-blue-400 hover:bg-blue-100 transition-all rounded px-2 py-1">
-                  Update
-                </button>
+              <div className="flex transition-all">
+                {isEditingAvatar ? (
+                  <>
+                    <button
+                      className="text-blue-500 outline-none border border-transparent hover:border-blue-400 hover:bg-blue-100 transition-all rounded px-2 py-1 disabled:opacity-50 disabled:cursor-not-allowed"
+                      onClick={() => setIsEditingAvatar(false)}
+                      disabled={isLoadingOnUpdateMe}
+                    >
+                      Cancel
+                    </button>
+                    <div className="border border-gray-300 mx-1"></div>
+                    <button
+                      className="text-blue-500 outline-none border border-transparent hover:border-blue-400 hover:bg-blue-100 transition-all rounded px-2 py-1 disabled:opacity-50 disabled:cursor-not-allowed"
+                      disabled={isLoadingOnUpdateMe}
+                      onClick={handleUpdateMeAvatar}
+                    >
+                      {isLoadingOnUpdateMe ? <Loader /> : 'Save'}
+                    </button>
+                  </>
+                ) : (
+                  <button
+                    className="text-blue-500 outline-none border border-transparent hover:border-blue-400 hover:bg-blue-100 transition-all rounded px-2 py-1"
+                    onClick={() => setIsEditingAvatar(true)}
+                  >
+                    Edit
+                  </button>
+                )}
               </div>
             </div>
 
